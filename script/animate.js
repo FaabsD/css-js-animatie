acf0d22ffe75126c26efbbf58c696f291c44d0f7
Fabian <19959@ma-web.nl>
2020-12-02 09:46:00 +0100

    trigger animatie na laden pagina

--- script/animate.js
+++ script/animate.js
@@ -2,3 +2,8 @@
 document.querySelector(".animate-button").addEventListener("click", (e) => {
     document.querySelector(".block").classList.toggle("block__to-right")
 })
+
+// na een korte tijd de animatie startend
+setTimeout(() => {
+    document.querySelector(".block").classList.toggle("block__to-right")
+}, 3000)
