3de43a1e4eea36d96f86c1705a518ca6fa66a283
Fabian <19959@ma-web.nl>
2020-12-02 10:25:36 +0100

    toggle de class navigation__link--slide in wanneer je op de menu knop drukt

--- script/animate.js
+++ script/animate.js
@@ -1,9 +1,21 @@
 // klik op animeer knop om het blockje te laten animeren
 document.querySelector(".animate-button").addEventListener("click", (e) => {
     document.querySelector(".block").classList.toggle("block__to-right")
-})
+});
 
 // na een korte tijd de animatie startend
 setTimeout(() => {
     document.querySelector(".block").classList.toggle("block__to-right")
-}, 3000)
+}, 3000);
+
+// klik op menu knop om links in te laten schuiven
+const menuButton = document.querySelector(".navigation__button");
+const navLinks = document.querySelectorAll(".navigation__link");
+
+menuButton.addEventListener("click", () => {
+    navLinks.forEach((item,index) => {
+        setTimeout(() => {
+            item.classList.toggle("navigation__link--slide-in");
+        }, 100 * index);
+    })
+});
